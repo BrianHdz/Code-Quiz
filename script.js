d5ac7cd89496035810684e55f2dc61da20f4fb46
BrianHdz <popcorn46290@sbcglobal.net>
2019-12-15 22:05:23 -0800

    elements will now hide. quiz begins when pressing start. Still need to randomize questions and add the interval on the top right.

--- script.js
+++ script.js
@@ -1,2 +1,81 @@
 document.querySelector(".time").innerHTML = "Time: 0";
-document.querySelector("#yourFinal").innerHTML = "Your final score is 0.";
+document.querySelector("#finalScore").innerHTML = "Your final score is 0.";
+
+var startWin = document.querySelector(".startWindow");
+var questionWin = document.querySelector(".questionWindow");
+
+var question = document.querySelector(".questionText");
+var choices = Array.from(document.querySelector(".choiceText"));
+console.log(choices);
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+
+
+function startQuiz() {
+    if (startWin.display === "none") {
+        startWin.style.display = "block";
+    } else {
+        startWin.style.display = "none";
+        questionWin.style.display = "block";
+    }
+}
+
+
+
+var questions = [
+    {
+        question: "Questean 1",
+
+        choiceOne: "1",
+        choiceTwo: "2",
+        choiceThree: "3",
+        choiceFour: "4",
+
+        answer: "1",
+    },
+    {
+        question: "Questean 2",
+
+        choiceOne: "1",
+        choiceTwo: "2",
+        choiceThree: "3",
+        choiceFour: "4",
+
+        answer: "1",
+    },
+    {
+        question: "Questean 3",
+
+        choiceOne: "1",
+        choiceTwo: "2",
+        choiceThree: "3",
+        choiceFour: "4",
+
+        answer: "1",
+    },
+    {
+        question: "Questean 4",
+
+        choiceOne: "1",
+        choiceTwo: "2",
+        choiceThree: "3",
+        choiceFour: "4",
+
+        answer: "1",
+    },
+    {
+        question: "Questean 5",
+
+        choiceOne: "1",
+        choiceTwo: "2",
+        choiceThree: "3",
+        choiceFour: "4",
+
+        answer: "1",
+    },
+];
